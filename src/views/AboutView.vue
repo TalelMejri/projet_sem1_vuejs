48b2a2d4f0e74523e0122995594e9aebcf6173ff
talel <talelmejri8@gmail.com>
2022-10-31 21:31:37 +0100

    begin for store

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,32 @@
 <template>
     <div class="contact">
       <div class="container-fluid py-5 " >
-          <div class="d-flex py-5  ">
-            <button class="btn btn-primary" @click="start">Start</button>
-            <button  class="btn btn-primary" @click="stop">Stop</button>
+          <div class="d-flex py-5 justify-content-center">
+            <div class="col-xl-12 col-lg-9 container">
+            <div class="row  rounded shadow-lg p-5">
+                <div class="col-lg-2 border-end">
+                      <h1>Menu :</h1>
+                      <div>
+                          <p>price :</p>
+                          <input type="range" value="50">
+                      </div>
+                      <div>
+
+                      </div>
+                </div>
+                <div class="col-lg-10 text-center">
+                      <div>
+                           <h1 class="text-center">T-shirt</h1>
+
+                      </div>
+                </div>
+              </div>
+            </div>
           </div>
       </div>
   </div>
+      <!--<button class="btn btn-primary" @click="start">Start</button>
+            <button  class="btn btn-primary" @click="stop">Stop</button>-->
 </template>
 
 <script>
