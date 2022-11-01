ef716447c3e1cce228321d08dd0ac2851116e6ad
talel <talelmejri8@gmail.com>
2022-11-01 19:24:30 +0100

    clear default

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -2,7 +2,7 @@
   <div class="navbar">
   <nav class="mb-5 navbar navbar-expand-lg text-uppercase fixed-top ">
     <div class="container">
-        <a class="navbar-brand" href="##">Bizo Shop</a>
+        <a class="navbar-brand" href="##">Vue Shop</a>
         <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             Menu
             <i class="fas fa-bars"></i>
