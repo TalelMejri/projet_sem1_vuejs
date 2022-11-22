b9ad5f880e93dff426bf8576832fa6dc87e64ed1
talel <talelmejri8@gmail.com>
2022-11-22 10:34:00 +0100

    add event with api

---
+++ src/components/Eventcard.vue
@@ -0,0 +1,57 @@
+<template>
+        <div class="hello container py-5">
+            <h1 class="mt-5 text-center">List Events :</h1>
+            <div class="row mb-4 d-flex justify-content-center py-3" >
+              <div v-if="events==''" class="w-50">
+                 <div class="alert alert-danger">
+                     Empty Events
+                 </div>
+              </div>
+              <div v-else v-for="event in events" :key="event.id" class="col-md-6 mb-2 ">
+                    <div class="card ">
+                         <div class="card-header text-center">
+                           {{event.category + event.id}}
+                         </div>
+                         <div class="card-body w-100 text-center font-weight-bold">
+                          <div class="card-text">  {{event.category }} </div>
+                          <div class="card-text">  {{event.title}}</div>
+                          <div class="card-text">  {{event.description}}</div>
+                          <div class="card-text">  {{event.location}}</div>
+                          <div class="card-text">  {{event.time}}</div>
+                         </div>
+                         <div class="p-3 float-end">
+                            <button @click="editevent(event.id)" class="btn btn-warning mx-2">Edit</button>
+                            <button @click="delete_event(event)" class="btn btn-danger">delete</button>
+                         </div>
+                    </div>
+              </div>
+            </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data(){
+      return{
+       // events:[]
+      }
+  },
+  props:{
+    events:Object
+  },
+  methods:{
+    delete_event(index){
+        if(confirm("do you want delete this")){
+            this.$emit('deleteevnt',index);
+        }
+    }
+  }
+
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped >
+
+</style>
---
+++ src/views/TestEventView.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <listesvent :events="events" @deleteevnt="deleteevnt"></listesvent>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+import listesvent from "@/components/Eventcard.vue";
+export default{
+  name:'TestEventView',
+  data(){
+   return {
+    events:[]
+   }
+  }, 
+  components:{
+    listesvent
+  },
+  created(){
+
+axios.get('https://my-json-server.typicode.com/TalelMejri/api_calls/events').then(
+  response=>{
+    console.log(response.data);
+    this.events=response.data;
+  }
+).catch(response=>{
+  console.log(response);
+})
+},
+methods:{
+deleteevnt(event){
+      let item=this.events.find((v)=>v.id==event.id);
+       let i=this.events.indexOf(item);
+       this.events.splice(i,1);
+}
+}
+}
+</script>
